c2ec3da81a6ceabbcf6cf2c44b863410c0d50d20
Luca D'Angelo <info@lucadangelo.it>
2024-04-17 06:04:22 +0100

    Statis Node Connection, pulling Inv, looping Vectors, printing event type and hash at console

--- ss3/Controllers/HomeController.cs
+++ ss3/Controllers/HomeController.cs
@@ -1,6 +1,13 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.AspNetCore.Mvc;
 using ss3.Models;
 using System.Diagnostics;
+using System.Net;
+using System.Xml.Linq;
+
+using NBitcoin;
+using NBitcoin.Protocol;
+
 
 namespace ss3.Controllers
 {
@@ -15,7 +22,64 @@ namespace ss3.Controllers
 
         public IActionResult Index()
         {
-            return View();
+
+            try
+            {
+                IPAddress selectedAddress = IPAddress.Parse("66.94.117.48"); //LD this is a working node
+                Console.WriteLine($" 000 IP {selectedAddress}");
+
+                var peerEndPoint = new IPEndPoint(selectedAddress, 8333); //LD bitcoin port is usually this one
+
+                //LD connection to node
+                using (var node = Node.Connect(Network.Main, peerEndPoint))
+                {
+                    //LD any changing state
+                    node.StateChanged += (sender, state) =>
+                    {
+                        Console.WriteLine($"LD Node Sate: {state}");
+                    };
+
+                    //LD handling any received message
+                    node.MessageReceived += (sender, e) =>
+                    {
+                        if (e.Message.Payload is InvPayload invPayload) //LD check if inv
+                        {
+                            foreach (InventoryVector item in invPayload.Inventory)//LD looping in vectors
+                            {
+                                string eventType = item.Type switch
+                                {
+                                    InventoryType.MSG_TX => "Transaction",
+                                    InventoryType.MSG_BLOCK => "Block", //LD hash related to data block. That's what I need
+                                    _ => "Unknown"
+                                };
+                                Console.WriteLine($" 001 Received {eventType} event: {item.Hash}");
+                            }
+                        }
+                        else
+                        {
+                            //LD any other message
+                            Console.WriteLine($"LD OTHER Received message: {e.Message.Payload}");
+
+                            var receivedMessage = e.Message.Payload.GetType().Name;
+                            Console.WriteLine($"LD OTHER Received message type: {receivedMessage}");
+                        }
+                    };
+
+                    //LD vesron handshake
+                    node.VersionHandshake();
+
+                    Console.ReadLine();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"ERROR: {ex.Message}");
+                return StatusCode(500, "Internal Server Error");
+            }
+
+            return StatusCode(500, "Internal Server Error");
+
+            //return View();
         }
 
         public IActionResult Privacy()
