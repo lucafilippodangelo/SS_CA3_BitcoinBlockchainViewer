ddf7f15ae28cf167c096972043d1c878fcca742b
Luca D'Angelo <info@lucadangelo.it>
2024-04-28 16:58:13 +0100

    UI, WIP bootstrap changes to use fluid containers grid

--- ss3_react/ss3-react-app/src/App.js
+++ ss3_react/ss3-react-app/src/App.js
@@ -1,4 +1,3 @@
-
 import React, { useState, useEffect } from 'react';
 import BitcoinEvents from './components/BitcoinEvents';
 import { Table, Container, Row, Col, Form, Pagination } from 'react-bootstrap';
@@ -54,73 +53,95 @@ const CustomPagination = ({ currentPage, totalPages, onPageChange }) => {
   );
 };
 
-
 const BlockQueueTable = ({ blockQueue }) => {
-    const [paginationStatus, setPaginationStatus] = useState({});
-    const [totalPages, setTotalPages] = useState({}); 
-    const itemsPerPage = 10;
+  const [paginationStatus, setPaginationStatus] = useState({});
+  const [totalPages, setTotalPages] = useState({});
+  const itemsPerPage = 10;
 
-    useEffect(() => {
-      const updatedTotalPages = {};
-      blockQueue.forEach(block => {
-        const totalTransactions = block.content.Transactions.length;
-        updatedTotalPages[block.content.Hash] = Math.ceil(totalTransactions / itemsPerPage); 
-      });
-      setTotalPages(updatedTotalPages);
-    }, [blockQueue]);
-    
-    const handlePaginationClick = (hash, pageNumber) => {
-      setPaginationStatus(prevStatus => ({
-        ...prevStatus,
-        [hash]: pageNumber
-      }));
-    };
-  
-    return (
-      <Container>
-        {blockQueue.map((block, index) => (
-          <div key={index}>
-            <h2>Block {block.content.Hash}</h2>
-            <p>Timestamp: {block.content.Timestamp}</p>
-            <p>Nonc kkk e: {block.content.Nonce}</p>
-            <p>Difficulty: {block.content.Difficulty}</p>
-            <p>Hash Verification: {block.content.HashVerification}</p>
-  
-            <Table striped bordered hover>
-              <thead>
-                <tr>
-                  <th>Transaction ID</th>
-                  <th>Total Value</th>
-                </tr>
-              </thead>
-              <tbody>
-                {block.content.Transactions.slice((paginationStatus[block.content.Hash] || 1) * itemsPerPage - itemsPerPage, (paginationStatus[block.content.Hash] || 1) * itemsPerPage).map((tx, txIndex) => (
-                  <tr key={txIndex}>
-                    <td>{tx.TransactionId}</td>
-                    <td>{tx.TotalValue}</td>
-                  </tr>
-                ))}
-              </tbody>
-            </Table>
-  
-            <CustomPagination
-              currentPage={paginationStatus[block.content.Hash] || 1}
-              totalPages={totalPages[block.content.Hash] || 1} 
-              onPageChange={(pageNumber) => handlePaginationClick(block.content.Hash, pageNumber)}
-            />
-          </div>
-        ))}
-      </Container>
-    );
+  useEffect(() => {
+    const updatedTotalPages = {};
+    blockQueue.forEach(block => {
+      const totalTransactions = block.content.Transactions.length;
+      updatedTotalPages[block.content.Hash] = Math.ceil(totalTransactions / itemsPerPage);
+    });
+    setTotalPages(updatedTotalPages);
+  }, [blockQueue]);
+
+  const handlePaginationClick = (hash, pageNumber) => {
+    setPaginationStatus(prevStatus => ({
+      ...prevStatus,
+      [hash]: pageNumber
+    }));
   };
 
-  
+  return (
+    <Container fluid>
+      {blockQueue.map((block, index) => (
+        <div key={index}>
+          <Row>
+          <Col md={12}>
+            {/* LD some space */}
+            <div style={{ height: '50px' }}></div>
+          </Col>
+        </Row>
+          <Row>
+            <Col md={12}>
+              <h6>Block: {block.content.Hash}</h6>
+            </Col>
+          </Row>
+          <Row>
+            <Col md={6}>
+              <p>Timestamp: {block.content.Timestamp}</p>
+            </Col>
+            <Col md={6}>
+              <p>Nonce: {block.content.Nonce}</p>
+            </Col>
+          </Row>
+          <Row>
+            <Col md={6}>
+              <p>Difficulty: {block.content.Difficulty}</p>
+            </Col>
+            <Col md={6}>
+              <p>Hash Verification: {block.content.HashVerification}</p>
+            </Col>
+          </Row>
+          <Row>
+            <Col md={12}>
+              <Table striped bordered hover>
+                <thead>
+                  <tr>
+                    <th>Transaction ID</th>
+                    <th>Total Value</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {block.content.Transactions.slice((paginationStatus[block.content.Hash] || 1) * itemsPerPage - itemsPerPage, (paginationStatus[block.content.Hash] || 1) * itemsPerPage).map((tx, txIndex) => (
+                    <tr key={txIndex}>
+                      <td>{tx.TransactionId}</td>
+                      <td>{tx.TotalValue}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </Table>
+            </Col>
+          </Row>
+          <Row>
+            <Col md={12}>
+              <CustomPagination
+                currentPage={paginationStatus[block.content.Hash] || 1}
+                totalPages={totalPages[block.content.Hash] || 1}
+                onPageChange={(pageNumber) => handlePaginationClick(block.content.Hash, pageNumber)}
+              />
+            </Col>
+          </Row>
+          <row>
 
-
-
-
-
-  
+          </row>
+        </div>
+      ))}
+    </Container>
+  );
+};
 
 function App() {
   const [latestBitcoinEvents, setLatestBitcoinEvents] = useState([]);
@@ -150,26 +171,34 @@ function App() {
     <div className="App">
       <BitcoinEvents onNewEvent={handleNewEvent} />
 
-      <h1>Bitcoin Transactions</h1>
+      <Container fluid>
+        <Row>
+          <Col md={12}>
+            <h1 style={{ textAlign: 'center' }}>Bitcoin Transactions</h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={12}>
+            <Table striped bordered hover style={{ width: '90%', margin: 'auto' }}>
+              <thead>
+                <tr>
+                  <th>Event Type</th>
+                  <th>Hash</th>
+                </tr>
+              </thead>
+              <tbody>
+                {latestBitcoinEvents.map((event, index) => (
+                  <tr key={index}>
+                    <td>{event.eventType}</td>
+                    <td>{event.hash}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </Table>
+          </Col>
+        </Row>
+      </Container>
 
-        <Table striped bordered hover>
-          <thead>
-            <tr>
-              <th>Event Type</th>
-              <th>Hash</th>
-            </tr>
-          </thead>
-          <tbody>
-            {latestBitcoinEvents.map((event, index) => (
-              <tr key={index}>
-                <td>{event.eventType}</td>
-                <td>{event.hash}</td>
-              </tr>
-            ))}
-          </tbody>
-        </Table>
-
-      <h1>Block Events</h1>
       <BlockQueueTable blockQueue={blockQueue} />
     </div>
   );
